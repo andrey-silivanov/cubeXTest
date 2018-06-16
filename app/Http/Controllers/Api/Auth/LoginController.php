35e2b28939d252726d327ea8ff362a9749c22799
a_silivanov <andrey.silivanovv@gmail.com>
2018-06-16 16:14:57 +0300

    login

---
+++ app/Http/Controllers/Api/Auth/LoginController.php
@@ -0,0 +1,48 @@
+<?php
+declare (strict_types = 1);
+
+namespace App\Http\Controllers\Api\Auth;
+
+use App\Http\Controllers\Api\ApiController,
+    App\Http\Resources\UserResource,
+    App\Models\User,
+    Illuminate\Foundation\Auth\AuthenticatesUsers,
+    Illuminate\Http\Request,
+    Tymon\JWTAuth\Facades\JWTAuth,
+    Illuminate\Http\JsonResponse;
+
+/**
+ * Class LoginController
+ * @package App\Http\Controllers\Api\Auth
+ */
+class LoginController extends ApiController
+{
+    use AuthenticatesUsers;
+
+    /**
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse|\Symfony\Component\HttpFoundation\Response
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function login(Request $request)
+    {
+        $this->validateLogin($request);
+        if ($this->attemptLogin($request)) {
+            return $this->authenticated(auth()->user());
+        }
+
+        return $this->sendFailedLoginResponse($request);
+    }
+
+    /**
+     * @param User $user
+     * @return \Illuminate\Http\JsonResponse
+     */
+    protected function authenticated(User $user): JsonResponse
+    {
+        return $this->successResponse(
+            $this->transformDataForResponse(new UserResource($user)), trans('auth.login'),
+            ['access_token' => JWTAuth::fromUser($user)]
+        );
+    }
+}
