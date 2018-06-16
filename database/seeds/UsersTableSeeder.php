ec59146bbdc7f366d6833d2c2d998c111b615395
a_silivanov <andrey.silivanovv@gmail.com>
2018-06-16 17:29:32 +0300

    user seede

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(User::class)->create([
+            'name' => 'John',
+            'email' => 'test@mail.com',
+        ]);
+    }
+}
