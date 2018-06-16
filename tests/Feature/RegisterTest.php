e306f6f68fd54684dd63948a9c2fa6ce8340735b
a_silivanov <andrey.silivanovv@gmail.com>
2018-06-16 15:15:03 +0300

    validation test

--- tests/Feature/RegisterTest.php
+++ tests/Feature/RegisterTest.php
@@ -7,9 +7,14 @@ use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
+/**
+ * Class RegisterTest
+ * @package Tests\Feature
+ */
 class RegisterTest extends TestCase
 {
     use RefreshDatabase;
+
     /**
      * Register user
      * @test
@@ -21,8 +26,8 @@ class RegisterTest extends TestCase
         $password = '123456';
 
         $response = $this->postJson(route('register'), [
-            'name'  => $name,
-            'email' => $email,
+            'name'     => $name,
+            'email'    => $email,
             'password' => $password
         ]);
 
@@ -34,5 +39,44 @@ class RegisterTest extends TestCase
         $this->assertEquals(trans('auth.register'), $response->json('message'));
         $response->assertSuccessful();
         $response->assertHeader('access_token');
+    }
+
+    /**
+     * @param $params
+     * @param $errors
+     *
+     * @test
+     * @dataProvider providerValidation
+     */
+    public function validation($params, $errors)
+    {
+        $this->postJson(route('register'), $params)
+            ->assertJsonValidationErrors($errors);
+    }
+
+    public function providerValidation()
+    {
+        return [
+            'required email'     => [
+                ['name' => 'John', 'password' => '123456', 'password_confirmation' => '123456'],
+                ['email']
+            ],
+            'invalid email'      => [
+                ['name' => 'John', 'password' => '123456', 'password_confirmation' => '123456', 'email' => 'test'],
+                ['email']
+            ],
+            'required name'      => [
+                ['email' => 'test@mail.com', 'password' => '123456', 'password_confirmation' => '123456'],
+                ['name']
+            ],
+            'required password'  => [
+                ['email' => 'test@mail.com', 'name' => 'John', 'password_confirmation' => '123456'],
+                ['password']
+            ],
+            'confirmed password' => [
+                ['email' => 'test@mail.com', 'name' => 'John', 'password_confirmation' => '123456'],
+                ['password']
+            ],
+        ];
     }
 }
