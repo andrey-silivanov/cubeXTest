419c4c88ce3a494a4661590ff74b9979502e3f38
a_silivanov <andrey.silivanovv@gmail.com>
2018-06-17 15:29:40 +0300

    fixed jwt package

--- app/Models/User.php
+++ app/Models/User.php
@@ -14,7 +14,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
  * @property-read \Illuminate\Notifications\DatabaseNotificationCollection|\Illuminate\Notifications\DatabaseNotification[] $notifications
  * @mixin \Eloquent
  */
-class User extends Authenticatable implements JWTSubject
+class User extends Authenticatable
 {
     use Notifiable;
     /**
@@ -83,24 +83,4 @@ class User extends Authenticatable implements JWTSubject
     /**
      * Entity public methods go below
      */
-
-    /**
-     * Get the identifier that will be stored in the subject claim of the JWT.
-     *
-     * @return mixed
-     */
-    public function getJWTIdentifier()
-    {
-        return $this->getKey();
-    }
-
-    /**
-     * Return a key value array, containing any custom claims to be added to the JWT.
-     *
-     * @return array
-     */
-    public function getJWTCustomClaims()
-    {
-        return [];
-    }
 }
