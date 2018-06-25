b19aea2028772d3d10a2922c62ad6333c24f50db
a_silivanov <andrey.silivanovv@gmail.com>
2018-06-25 09:37:13 +0300

    fixed menu

--- resources/assets/js/components/navbar.vue
+++ resources/assets/js/components/navbar.vue
@@ -1,21 +1,25 @@
 <template>
     <vs-topbar vs-color="rgb(30, 199, 135)" id="topBar" ref="topBar">
         <h4>Test</h4>
+
+        <router-link v-if="$auth.user().role == 'manager'" :to="{name: 'admin'}" class="nav-link" active-class="active"> Messages  </router-link>
         <a href="#" @click.prevent="$auth.logout()">
             <vs-button v-if="$auth.check()" vs-color-text="rgb(40, 40, 40)" vs-color="rgba(40, 40, 40)"
                        vs-type="dark-flat">
                 Logout
             </vs-button>
         </a>
-        <router-link :to="{name: 'admin'}" class="nav-link" active-class="active"> Admin  </router-link>
-        <router-link :to="{name: 'user'}" class="nav-link" active-class="active"> Home </router-link>
     </vs-topbar>
 </template>
 <script type="text/babel">
-    export default ({})
+    export default ({
+    })
 </script>
 <style scoped>
     .vs-button-text{
         color: #232323!important
+    }
+    a {
+        color: #ffffff
     }
 </style>
